9077df20cd04c15fdab22c6ff55808830b2769e3
John Connor <john.connor@jirafe.com>
2013-08-12 11:52:00 -0400

    Handles broker disconnects.
    The bug:
    When a producer is connected to a broker, and is not transmitting
    information, and the connection is interrupted, (for example by a load
    balancer rebooting) the producer is not aware of this interruption, and
    when asked to send a message to the broker, it will do so without
    reconnecting.  The broker will then simply discard the message and send
    a RST tcp packet, which the producer will ignore.
    
    The fix:
    The producer now checks for RST packets, and throws a ConnectionReset
    exception when it receives one.  This allows the code calling the
    producer to handle the exception in a sane manner.

--- kafka/io.py
+++ kafka/io.py
@@ -2,6 +2,12 @@ import array
 import errno
 import socket
 
+
+class ConnectionResetException(Exception):
+  """Thrown when a `RST` packet is recieved from broker."""
+  pass
+
+
 class IO(object):
   """ Base class for handling socket communication to the Kafka server. """
 
@@ -65,5 +71,26 @@ class IO(object):
     while write_length > wrote_length:
       remainder = data[wrote_length:]
       wrote_length += self.socket.send(remainder)
-
+      self._check_reset()
     return wrote_length
+
+  def _check_reset(self):
+    """Check if we have received a RST packet from the broker.
+
+    According to the Kafka protocol, the only situation in which the broker
+    will send anything other than a TCP ACK is when the connection is
+    dropped, in which case the next time the producer sends to the broker,
+    the broker will send a TCP RST packet.
+
+    When a TCP RST packet is sent, it causes a RECV call to complete, and
+    "read" an empty string.
+    """
+    try:
+      self.socket.recv(1, socket.MSG_DONTWAIT)
+      raise ConnectionResetException()
+    except socket.error, e:
+      err = e.args[0]
+      if not err in (errno.EAGAIN, errno.EWOULDBOCK):
+        logging.exception("socket.error")
+        raise
+
